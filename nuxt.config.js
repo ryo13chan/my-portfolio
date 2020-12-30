dadf9e7c68d6280c4a52b5c201b52e5901206686
Ryo <>
2020-12-30 16:01:30 +0900

    Gitから最新のコミットを取得してヘッダーに表示する

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,9 @@ export default {
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [],
+  modules: ['@nuxtjs/axios'],
+
+  axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
