9541c2f442eaa28f4a5d5df9e739683b2ea80bd9
Ryo <>
2021-01-02 00:17:05 +0900

    コンポーネント集に検索ボックスを追加

--- components/components/SideMenu.vue
+++ components/components/SideMenu.vue
@@ -1,9 +1,21 @@
 <template>
   <aside class="side-menu">
-    <p class="side-menu__title">コンポーネント集</p>
+    <p class="side-menu__title">
+      <nuxt-link class="side-menu__link" to="/works/components">
+        コンポーネント集
+      </nuxt-link>
+    </p>
     <ul class="side-menu__list">
+      <li v-if="isDisplaySearchBox()" class="side-menu__search-box">
+        <input
+          v-model="searchWords"
+          type="search"
+          class="side-menu__search-input"
+          placeholder="コンポーネント名を検索"
+        />
+      </li>
       <li
-        v-for="menu in menuList"
+        v-for="menu in filteredMenuList"
         :key="menu.title"
         class="side-menu__item"
         :class="{ 'side-menu__item--active': isCurrentPage(menu.to) }"
@@ -20,20 +32,38 @@
 import Vue from 'vue'
 
 interface Data {
-  menuList: Array<Object>
+  searchWords: string
+  menuList: Array<Menu>
+}
+interface Menu {
+  title: string
+  to: string
 }
 
 export default Vue.extend({
   components: {},
   data(): Data {
     return {
+      searchWords: '',
       menuList: [
         { title: 'ボタン', to: '/works/components/button' },
         { title: 'モーダル', to: '/works/components/modal' },
       ],
     }
   },
+  computed: {
+    filteredMenuList(): Array<Menu> {
+      return this.menuList.filter((menu) =>
+        menu.title.includes(this.searchWords)
+      )
+    },
+  },
   methods: {
+    // 検索ボックスを表示するか
+    isDisplaySearchBox() {
+      // コンポーネント集ページのみ表示
+      return this.$route.path === '/works/components'
+    },
     // 現在のページか
     isCurrentPage(to: string): boolean {
       return to === this.$route.path
@@ -45,11 +75,29 @@ export default Vue.extend({
 <style lang="scss" scoped>
 @import '~/assets/scss/_variable.scss';
 
+::placeholder {
+  color: lighten($color: $base-color, $amount: 20%);
+}
+
 .side-menu {
   width: 300px;
   padding: 20px 20px 0;
   &__title {
     font-weight: bold;
+  }
+  &__search-box {
+    height: 3rem;
+    margin-bottom: 20px;
+  }
+  &__search-input {
+    height: 100%;
+    width: 95%;
+    padding-left: 20px;
+    border-radius: 9999px;
+    color: $text-color;
+    border: 1px solid $base-color;
+    outline: none;
+    font-size: 1rem;
   }
   &__list {
     list-style: none;
---
+++ pages/works/components/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <article>
+      <section class="content">
+        <AppHeading
+          class="content__title"
+          :level="2"
+          :text="'コンポーネント集'"
+        />
+        <p class="content__text">
+          ポートフォリオ内で使用しているコンポーネントの一覧です。
+        </p>
+      </section>
+    </article>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import AppHeading from '~/components/AppHeading.vue'
+
+export default Vue.extend({
+  layout: 'components',
+  components: {
+    AppHeading,
+  },
+  data() {
+    return {}
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/_variable.scss';
+.content {
+  max-width: 1230px;
+  padding: 20px 15px;
+  margin-right: auto;
+  margin-left: auto;
+  &__text {
+    font-size: 1.5rem;
+  }
+}
+</style>
