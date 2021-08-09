5375b0de1ad132455ba7b2c80ec6b956bc77b383
Ryosuke Narita <>
2021-08-10 06:03:32 +0900

    aboutの見出しをざっくり作成

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -3,6 +3,45 @@
     <article>
       <section class="content">
         <AppHeading :level="2" :text="'About'" />
+        <section>
+          <AppHeading :level="3" text="当サイトについて" />
+          <div class="content__about-this-site">
+            <div>ポートフォリオ</div>
+            <div>作成目的</div>
+            <div>使用技術</div>
+          </div>
+        </section>
+        <section>
+          <AppHeading :level="3" text="自己紹介" />
+          <div class="content__self-introduction">
+            json
+          </div>
+        </section>
+        <section>
+          <AppHeading :level="3" text="経歴" />
+          <div class="content__work-history">
+            2019/05 - 2020/01
+            <AppAccordion title="あああああシステム開発">
+              <template v-slot:body>
+                <div>
+                  <p>アコーディオン中身</p>
+                  <p>アコーディオン中身</p>
+                  <p>アコーディオン中身</p>
+                </div>
+              </template>
+            </AppAccordion>
+            2019/05 - 2020/01
+            <AppAccordion title="あああああシステム開発">
+              <template v-slot:body>
+                <div>
+                  <p>アコーディオン中身</p>
+                  <p>アコーディオン中身</p>
+                  <p>アコーディオン中身</p>
+                </div>
+              </template>
+            </AppAccordion>
+          </div>
+        </section>
       </section>
     </article>
   </main>
@@ -25,5 +64,10 @@ export default Vue.extend({
   padding: 20px 15px;
   margin-right: auto;
   margin-left: auto;
+
+  &__about-this-site {
+    display: flex;
+    justify-content: space-between;
+  }
 }
 </style>
